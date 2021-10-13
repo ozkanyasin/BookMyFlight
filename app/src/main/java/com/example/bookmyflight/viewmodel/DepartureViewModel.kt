974ac83301c630033a8eb88ecb030196fb7ac49a
pirematur3 <36839831+pirematur3@users.noreply.github.com>
2021-10-13 17:10:45 +0300

    - filtrelemeler tamamlanamadı - room, coroutine ile ilgili bir şeyler deneniyor

--- app/src/main/java/com/example/bookmyflight/viewmodel/DepartureViewModel.kt
+++ app/src/main/java/com/example/bookmyflight/viewmodel/DepartureViewModel.kt
@@ -1,20 +1,27 @@
 package com.example.bookmyflight.viewmodel
 
+import android.app.Application
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.bookmyflight.mode.FlightModel
 import com.example.bookmyflight.model.AirlineModel
+import com.example.bookmyflight.service.repository.FlightDatabase
 import com.example.bookmyflight.service.repository.FlightRepository
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class DepartureViewModel(val repository: FlightRepository) : ViewModel() {
+class DepartureViewModel(val repository: FlightRepository,application: Application)
+    : BaseViewModel(application) {
 
     val flightList = MutableLiveData<FlightModel>()
     val flightLoading = MutableLiveData<Boolean>()
 
+    fun refreshData(){
+
+    }
 
     fun getFlights(){
 
@@ -34,8 +41,23 @@ class DepartureViewModel(val repository: FlightRepository) : ViewModel() {
 
     }
 
-    private fun storeInRoom(){
+    private fun showFlights(flightsList: FlightModel) {
+        flightList.value = flightsList
+    }
 
+    private fun storeInRoom(list : ArrayList<FlightModel>){
+        launch {
+            val dao = FlightDatabase(getApplication()).flightDao()
+            dao.deleteAllFlight()
+            val listLong = dao.insertAll(*list.toTypedArray()) // -> list -> individual
+            var i = 0
+            while (i<list.size){
+                list[i].uuid = listLong[i].toInt()
+                i++
+            }
+
+            showFlights(list[1])
+        }
     }
 
 }
